d23204d295957b11b7897c8b123ac2abce86e5f1
HoangHongNgo <honghoangngo@gmail.com>
2023-07-12 09:55:57 +0700

    array example part 2

--- src/array.js
+++ src/array.js
@@ -1,9 +1,15 @@
+/*
+  Fetch data
+*/
 getListUsers = async () => {
   const apiRequest = await fetch("users.json");
   const dataResponse = await apiRequest.json();
   return dataResponse;
 };
 
+/*
+  Process data, Execute functions
+*/
 processData = async () => {
   const data = await getListUsers();
   console.log("List of users: ", data);
@@ -15,8 +21,15 @@ processData = async () => {
   listNation(data);
 
   listFemaleUsers(data);
+
+  anyVietNamese(data);
+
+  everyHasJob(data);
 };
 
+/*
+  map method example
+*/
 setIsActive = (users) => {
   let newData = users.map((user) => {
     user.isActice = true;
@@ -25,6 +38,9 @@ setIsActive = (users) => {
   console.log("Update user status: ", newData);
 };
 
+/*
+  find method example
+*/
 findUserById = (id, users) => {
   console.log(
     "Looking for user " + id + "...",
@@ -32,6 +48,9 @@ findUserById = (id, users) => {
   );
 };
 
+/*
+  forEach, push method example
+*/
 listNation = (users) => {
   let nations = [];
   users.forEach((user) => {
@@ -40,12 +59,54 @@ listNation = (users) => {
   console.log("List of nations: ", nations);
 };
 
+/*
+  filter method example
+*/
 listFemaleUsers = (users) => {
   let females = users.filter((user) => user.sex === "female");
   console.log("List of female users: ", females);
 };
 
-const getMax = (a, b) => Math.max(a, b);
-console.log("Reduce example returns: ", [1, 100].reduce(getMax, 150));
+/*
+  some method example
+*/
+anyVietNamese = (users) => {
+  console.log("Is there any Vietnamese? ", users.some((user) => user.nation === "Viet Nam") ? "Yes" : "No");
+};
+
+/*
+  every method example
+*/
+everyHasJob = (users) => {
+  console.log("Does everyone has job? ", users.every((user) => user.job !== "") ? "Yes" : "No");
+};
 
 processData();
+
+/*
+  reduce method example
+*/
+const getMax = (a, b) => Math.max(a, b);
+console.log("Reduce example returns: ", [1, 50, 100].reduce(getMax, 75));
+
+/*
+  examples for push, pop, shift, unshift and sort methods
+*/
+const array = [0, 9, 3, 5, 4, 8, 7, 0, 3, 7];
+
+console.log("The array: ", array);
+
+console.log("Push method returns: ", array.push("end"));
+console.log("The array after push: ", array);
+
+console.log("Pop method returns: ", array.pop());
+console.log("The array after pop: ", array);
+
+console.log("Shift method returns: ", array.shift());
+console.log("The array after shift: ", array);
+
+console.log("Unshift method returns: ", array.unshift("+84"));
+console.log("The array after unshift: ", array);
+
+console.log("Sort method returns: ", array.sort());
+console.log("The array after sort: ", array);
